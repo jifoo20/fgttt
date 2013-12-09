3523371d1b05a54efb3ce0780090b5c089385cef
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2013-12-09 17:22:31 +0000

    Update #9

--- Untitled237/WebFolder/index.waPage/scripts/index.js
+++ Untitled237/WebFolder/index.waPage/scripts/index.js
@@ -2,10 +2,17 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var button2 = {};	// @button
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
+	button2.click = function button2_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+	};// @lock
+
 // @region eventManager// @startlock
+	WAF.addListener("button2", "click", button2.click, "WAF");
 // @endregion
 };// @endlock
